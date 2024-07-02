46440d0df37e8f5aa9212b0dd121981043d510b9
David Cadena <david.cadena@hotmail.com>
2024-07-02 11:09:29 -0500

    ✨ Feat: (PN-1) Create required models

---
+++ domain/src/models/client.py
@@ -0,0 +1,9 @@
+from typing import NamedTuple
+
+
+class Client(NamedTuple):
+    id: str
+    first_name: str
+    last_name: str
+    email: str
+    movil_number: str
---
+++ domain/src/models/product.py
@@ -0,0 +1,7 @@
+from typing import NamedTuple
+
+class Product(NamedTuple):
+    id: str
+    product_name: str
+    description: str
+    price: float
